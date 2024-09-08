2b9d57b050e40915b2d74a905298820590534d65
Xzeius <rameshsuday@gmail.com>
2024-09-08 14:02:16 +0530

    Siddhesh Knex file

--- config/knex.js
+++ config/knex.js
@@ -14,16 +14,32 @@
 
 
 
+// const knex = require('knex');
+
+// const db = knex({
+//     client: 'mysql2',
+//     connection: {
+//         host: process.env.host,
+//         user: process.env.user,
+//         password: process.env.password,
+//         database: process.env.database,
+//     },
+// });
+
+// module.exports = db;
+
+require('dotenv').config();
 const knex = require('knex');
 
 const db = knex({
     client: 'mysql2',
     connection: {
-        host: process.env.host,
-        user: process.env.user,
-        password: process.env.password,
-        database: process.env.database,
-    },
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }
 });
 
 module.exports = db;
